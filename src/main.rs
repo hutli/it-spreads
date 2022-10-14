9358c187c58560050b4e0b13700789ae84bac87a
hutli <jensstaermose@hotmail.com>
2022-10-14 15:13:18 +0200

    init

---
+++ src/main.rs
@@ -0,0 +1,33 @@
+use bevy::prelude::*;
+
+#[derive(Component)]
+struct Name(String);
+
+#[derive(Component)]
+struct Person;
+
+fn add_people(mut commands: Commands) {
+    commands.spawn().insert(Person).insert(Name("Elaina Proctor".to_string()));
+    commands.spawn().insert(Person).insert(Name("Renzo Hume".to_string()));
+    commands.spawn().insert(Person).insert(Name("Zayna Nieves".to_string()));
+}
+
+fn greet_people(query: Query<&Name, With<Person>>) {
+    for name in query.iter() {
+        println!("hello {}!", name.0);
+    }
+}
+
+fn hello_world() {
+    println!("hello world!");
+}
+
+
+
+fn main() {
+    App::new()
+        .add_startup_system(add_people)
+        .add_system(hello_world)
+        .add_system(greet_people)
+        .run();
+}
