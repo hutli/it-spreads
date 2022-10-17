165093367312f3eba7afe00a55e74cdb9ec0b150
hutli <jensstaermose@hotmail.com>
2022-10-17 22:20:30 +0200

    Initial map level generator of specks

--- src/main.rs
+++ src/main.rs
@@ -1,60 +1,69 @@
-use bevy::prelude::*;
+use bevy::{prelude::*, render::render_resource::encase::rts_array::Length};
+use rand::prelude::*;
 
-const BACKGROUND_COLOR: Color = Color::rgb(255.0, 0.0, 0.0);
+const BACKGROUND_COLOR: Color =
+    Color::rgb(0x87 as f32 / 256., 0xce as f32 / 256., 0xeb as f32 / 256.);
+const DIRT_COLORS: [Color; 5] = [
+    Color::rgb(0x40 as f32 / 256., 0x29 as f32 / 256., 0x05 as f32 / 256.),
+    Color::rgb(0x76 as f32 / 256., 0x55 as f32 / 256., 0x2b as f32 / 256.),
+    Color::rgb(0x6b as f32 / 256., 0x54 as f32 / 256., 0x28 as f32 / 256.),
+    Color::rgb(0xb6 as f32 / 256., 0x9f as f32 / 256., 0x66 as f32 / 256.),
+    Color::rgb(0xea as f32 / 256., 0xd0 as f32 / 256., 0xa8 as f32 / 256.),
+];
+
+const BRICK_COLOR: Color = Color::rgb(0.5, 0.5, 1.0);
+const SPECK_SIZE: Vec2 = Vec2::new(1., 1.);
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .insert_resource(ClearColor(BACKGROUND_COLOR))
         .add_startup_system(setup)
-        .add_plugin(HelloPlugin)
         .run();
 }
 
 #[derive(Component)]
-struct Name(String);
+struct Collider;
 
 #[derive(Component)]
-struct Person;
+struct Speck;
 
-fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
-    // Camera
-    commands.spawn_bundle(Camera2dBundle::default());
-}
+fn setup(mut commands: Commands, windows: Res<Windows>) {
+    for window in windows.iter() {
+        let left_border: i32 = -(window.physical_width() as i32) / 2;
+        let right_border: i32 = (window.physical_width() as i32) / 2;
+        let bottom_border: i32 = -(window.physical_height() as i32) / 2;
+        let top_border: i32 = (window.physical_height() as i32) / 2;
 
-fn add_people(mut commands: Commands) {
-    commands
-        .spawn()
-        .insert(Person)
-        .insert(Name("Elaina Proctor".to_string()));
-    commands
-        .spawn()
-        .insert(Person)
-        .insert(Name("Renzo Hume".to_string()));
-    commands
-        .spawn()
-        .insert(Person)
-        .insert(Name("Zayna Nieves".to_string()));
-}
-
-struct GreetTimer(Timer);
-
-fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
-    // update our timer with the time elapsed since the last update
-    // if that caused the timer to finish, we say hello to everyone
-    if timer.0.tick(time.delta()).just_finished() {
-        for name in query.iter() {
-            println!("hello {}!", name.0);
+        // Camera
+        commands.spawn_bundle(Camera2dBundle::default());
+        // brick
+        let mut height = 200;
+        let mut rng = rand::thread_rng();
+        let color_borders = (-bottom_border + top_border) / 5;
+        for x in left_border..right_border {
+            height = height + rng.gen::<i32>() % 2;
+            for y in bottom_border..height {
+                let brick_position = Vec2::new(x as f32, y as f32);
+                commands
+                    .spawn()
+                    .insert(Speck)
+                    .insert_bundle(SpriteBundle {
+                        sprite: Sprite {
+                            color: DIRT_COLORS[(((-bottom_border + y) / color_borders)
+                                % (DIRT_COLORS.length() as i32))
+                                as usize],
+                            ..default()
+                        },
+                        transform: Transform {
+                            translation: brick_position.extend(0.0),
+                            scale: Vec3::new(SPECK_SIZE.x, SPECK_SIZE.y, 1.0),
+                            ..default()
+                        },
+                        ..default()
+                    })
+                    .insert(Collider);
+            }
         }
-    }
-}
-
-pub struct HelloPlugin;
-
-impl Plugin for HelloPlugin {
-    fn build(&self, app: &mut App) {
-        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, true)))
-            .add_startup_system(add_people)
-            .add_system(greet_people);
     }
 }
